ad9fc32d73f5098660976aa00683edf1760b4612
Lina <61861965+l1na-forever@users.noreply.github.com>
2023-04-11 23:47:02 -0700

    revert change to old watermark

--- home/.local/bin/watermark264.sh
+++ home/.local/bin/watermark264.sh
@@ -21,14 +21,12 @@ output="$3"
 _PADDING="25"
 _SCALE="0.17"
 _ALPHA="0.8"
-_CRF="25"
 
 ffmpeg -i "$input" \
 			 -i "$watermark" \
-			 -filter_complex "[0]zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p[0p];[1][0p]scale2ref=w=oh*mdar:h=ih*$_SCALE[logo][video];[logo]format=rgba,colorchannelmixer=aa=$_ALPHA[logo];[video][logo]overlay=W-w-$_PADDING:H-h-$_PADDING:format=auto,format=yuv420p" \
-			 -c:v libx265 \
-			 -s "1080x1920" \
+			 -filter_complex "[1][0]scale2ref=w=oh*mdar:h=ih*$_SCALE[logo][video];[logo]format=rgba,colorchannelmixer=aa=$_ALPHA[logo];[video][logo]overlay=W-w-$_PADDING:H-h-$_PADDING:format=auto,format=yuv420p" \
+			 -c:v libx264 \
 			 -c:a copy \
 			 -preset slow \
-			 -crf "$_CRF" \
+			 -crf 28 \
 			 "$output"
